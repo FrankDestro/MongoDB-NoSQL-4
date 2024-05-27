7867c22697fe5cfc4edc158b19a74924ca8d92c7
FrankDestro <frank-destro@outlook.com>
2024-05-27 16:12:45 -0300

    feat: implement findById for Users

--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -4,11 +4,9 @@ import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.Path;
 import java.util.List;
 
 @RestController
@@ -22,5 +20,11 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> list = userService.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.devsuperior.workshopmongo.services;
 import com.devsuperior.workshopmongo.models.dto.UserDTO;
 import com.devsuperior.workshopmongo.models.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,9 +16,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public List<UserDTO> findAll () {
+    public List<UserDTO> findAll() {
         List<User> list = userRepository.findAll();
         return list.stream().map(user -> new UserDTO(user)).toList();
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> result = userRepository.findById(id);
+        User entity = result.orElseThrow(() -> new ResourceNotFoundException("Id not found"));
+        return new UserDTO(entity);
     }
 }
 
